fb9c3204f1022800034563ff317179e9445bca64
lsw <349986245@qq.com>
2018-11-18 13:03:41 +0800

    封装获取access—_token的方法

--- day01/03获取access_token/weiChat/access_token.js
+++ day01/03获取access_token/weiChat/access_token.js
@@ -92,7 +92,46 @@ class Wechat {
       return true
     }
   }
+
+  /**
+   * 返回有效的access_token的方法
+   * @returns {Promise.<{access_token: *, expires_in: (*|number)}>}
+   */
+  fetchAccessToken () {
+    if(this.access_token && this.expires_in && this.isValidAccessToken(this)) {
+      //说明access_token是有效的
+      return Promise.resolve({access_token:this.access_token,expires_in:this.expires_in})
+    }
+    //目的返回有效的access_token
+   return this.readAccessToken('./accessToken.txt')
+      .then(async res => {
+        if (this.isValidAccessToken(res)) {
+          //没有过期，直接使用
+          //then函数的返回值，promise对象包着res
+          return res;
+        } else {
+          //过期了
+          const accessToken = await this.getAccessToken();
+          await this.saveAccessToken('./accessToken.txt', accessToken);
+          //then函数的返回值，promise对象包着accessToken
+          return accessToken
+        }
+      })
+      .catch(async err => {
+        const accessToken = await this.getAccessToken();
+        await this.saveAccessToken('./accessToken.txt',accessToken)
+        return accessToken
+      })
+      .then(res => {
+        //不管上面成功或失败都会来到这
+        this.access_token = res.access_token;
+        this.expires_in = res.expires_in;
+        return Promise.resolve(res);
+      })
+  }
 };
+
+
 
 (async () => {
   /*
@@ -105,20 +144,8 @@ class Wechat {
    - 发送请求，获取access_token，保存下来
    */
     const w = new Wechat();
-    w.readAccessToken('./accessToken.txt')
-      .then(async res => {
-        if (w.isValidAccessToken(res)) {
-          //没有过期，直接使用
-          console.log(res);
-          console.log('没有过期，直接使用');
-        } else {
-          //过期了
-          const accessToken = await w.getAccessToken();
-          await w.saveAccessToken('./accessToken.txt', accessToken);
-        }
-      })
-      .catch(async err => {
-        const accessToken = await w.getAccessToken();
-        await w.saveAccessToken('./accessToken.txt',accessToken)
-      })
+    let result = await w.fetchAccessToken();
+    console.log(result);
+        result = await w.fetchAccessToken();
+    console.log(result)
 })();
